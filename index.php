5376129fad2406fca4bbbbb6d8fcc902b566f63a
choilapitan <choilapitan47@gmail.com>
2020-11-03 18:08:41 +0800

    I added index.php for adding a friend and posting a status

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-    echo "Hello World";
+    class User{
+        //PROPERTIES
+        public $name;
+        public $username;
+        public $email;
+
+        function addFriend($name) {
+            echo $name. " Added a friend.";
+        }
+        function postStatus($name) {
+            echo $name. " Posted a status.";
+        }
+    }
+        //METHOD
+        $example = new User();
+        echo $example->addFriend('Choi');
+        echo "<br>";
+        echo $example->postStatus('Choi');  
 ?>
